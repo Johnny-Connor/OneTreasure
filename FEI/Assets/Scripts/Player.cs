0ac1b6ed31b931f820d5ad6a314e911e1b2c92c6
João Victor Moreira <42523848+SupaMorris@users.noreply.github.com>
2021-04-24 09:25:41 -0300

    Movimentação do Gato
    Como a minha tarefa é parecida com a da Carol, tive que fazer a movimentação também...

---
+++ FEI/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{    
+    public float movementSpeed = 1f;
+    public Vector2 movement;
+    public Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() 
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+
+}
