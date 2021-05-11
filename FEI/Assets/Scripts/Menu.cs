d5836d3902adb530a533ffcc3ac275059a7b8b91
João Victor Moreira <42523848+SupaMorris@users.noreply.github.com>
2021-05-11 20:56:02 -0300

    Menu principal adicionado
    Gohan, vê se você me escuta, não é pecado lutar pela justiça ao contrario é uma boa ação, pense existem inimigos que não são convencidos com palavras, você só precisa soltar a fúria, que está dentro do seu espirito eu sei como você se sente não precisa suportar isso Gohan , Gohan proteja os seres vivos e as plantas deste mundo, que eu tanto amei. Conto com você.

---
+++ FEI/Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void jogar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void sair()
+    {
+        Application.Quit();
+    }
+}
